259e2a1386f3c5d4e57cb03cfe2963385d94ee3c
HJ-513 <hjchoai1305@gmail.com>
2023-07-11 15:52:17 +0900

    0711

--- StudentInfo/app/src/main/java/com/example/studentinfo/MainActivity.java
+++ StudentInfo/app/src/main/java/com/example/studentinfo/MainActivity.java
@@ -2,13 +2,55 @@ package com.example.studentinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView namedValueTextView,birthValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.editButton).setOnClickListener(this);
+        findViewById(R.id.deleteButton).setOnClickListener(this);
+        namedValueTextView = findViewById(R.id.nameValueTextView);
+        birthValueTextView = findViewById(R.id.birthValueTextView);
+
+        readData();
+    }
+
+    private void readData() {
+        SharedPreferences sp = getSharedPreferences(getString(R.string.sp), MODE_PRIVATE);
+        namedValueTextView.setText(sp.getString("이름","무명"));
+        birthValueTextView.setText(sp.getString("생일", "모름"));
+        // 혈액형, 기타정보 읽어서 화면에 표시
+        // to do
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.editButton:
+                Intent intent = new Intent(MainActivity.this, editActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.deleteButton:
+                SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.sp), MODE_PRIVATE).edit();
+                editor.clear();
+                editor.apply();
+                readData();
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        readData();
     }
 }
--- StudentInfo/app/src/main/java/com/example/studentinfo/editActivity.java
+++ StudentInfo/app/src/main/java/com/example/studentinfo/editActivity.java
@@ -2,13 +2,73 @@ package com.example.studentinfo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.telephony.TelephonyCallback;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class editActivity extends AppCompatActivity {
+public class editActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView namedValueTextView,birthValueTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        namedValueTextView = findViewById(R.id.nameValueTextView);
+        birthValueTextView = findViewById(R.id.birthValueTextView);
+
+        findViewById(R.id.birthDateLayer).setOnClickListener(this); // 날짜 레이어 클릭
+        findViewById(R.id.saveButton).setOnClickListener(this); // 저장버튼
+    }
+
+    private void readData() {
+        SharedPreferences sp = getSharedPreferences(getString(R.string.sp), MODE_PRIVATE);
+        namedValueTextView.setText(sp.getString("이름","무명"));
+        birthValueTextView.setText(sp.getString("생일", "모름"));
+        // 혈액형, 기타정보 읽어서 화면에 표시
+        // to do
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.birthDateLayer:
+                //데이트 피커 생성
+                DatePickerDialog.OnDateSetListener myDatePicker =
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                                ((TextView)findViewById(R.id.birthValueTextView)).setText(y+"/"+(m+1)+"/"+d);
+                            }
+                        };
+                DatePickerDialog dialog = new DatePickerDialog(this,myDatePicker, 2023, 6, 11);
+                dialog.show();
+                break;
+            case R.id.saveButton:
+                // 데이터 저장
+                saveDate();
+                break;
+        }
+    }
+
+    private void saveDate() {
+        // 이름과 날짜를 읽어서 sp에 저장한다.
+        String name = namedValueTextView.getText().toString();
+        String birth = birthValueTextView.getText().toString();
+        //s_p의 editor 열기
+        SharedPreferences.Editor editor = getSharedPreferences(getString(R.string.sp), MODE_PRIVATE).edit();
+        editor.putString("이름", name);
+        editor.putString("생일", birth);
+
+        // to do
+
+        editor.apply();
+
+        finish();
     }
 }
